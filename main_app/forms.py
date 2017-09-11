b2c66b55994c72a3e68b5de9583d798a297b7639
anton9113 <Afanasjevanton@gmail.com>
2017-09-11 17:19:18 +0300

    replace bad fix to correct fix

--- main_app/forms.py
+++ main_app/forms.py
@@ -14,10 +14,9 @@ ALPHABET = (
 
 def build_sectors_list():
 	choices = [('all', 'Все отделы')]
-	if 'makemigrations' not in sys.argv and 'migrate' not in sys.argv:
-		sectors = Sector.objects.all()
-		for i in sectors:
-			choices.append((i.pk, i.name))
+	sectors = Sector.objects.all()
+	for i in sectors:
+		choices.append((i.pk, i.name))
 	return choices
 
 class ProfileAdminForm(forms.ModelForm):
@@ -27,6 +26,14 @@ class ProfileAdminForm(forms.ModelForm):
 
 
 class ProfileFilterForm(forms.Form):
-	available = forms.BooleanField(label = 'Работают в компании', required=False, widget=forms.CheckboxInput(attrs={'name':'available', 'value':'1','style':'selector_element',}))
-	sector = forms.ChoiceField(label='Отдел', required=False, choices=build_sectors_list(), widget=forms.Select(attrs={'name':'sector', 'style':'selector_element',}))
+	def __init__(self, *args, **kwargs):
+		choices = [('all', 'Все отделы')]
+		sectors = Sector.objects.all()
+		for i in sectors:
+			choices.append((i.pk, i.name))
+		self.fields['sector'].choices=choices
 
+	available = forms.BooleanField(label = 'Работают в компании', required=False, \
+		widget=forms.CheckboxInput(attrs={'name':'available', 'value':'1','style':'selector_element',}))
+	sector = forms.ChoiceField(label='Отдел', required=False, \
+			widget=forms.Select(attrs={'name':'sector', 'style':'selector_element',}))
